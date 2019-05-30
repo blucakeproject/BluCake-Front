f7c6b1b3d23f79684a31cd6f5c42a635564b4de6
efsant0s <eddu.felipe@gmail.com>
2019-05-29 21:20:47 -0300

    Ajuste para testes do servidor

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: 'http://localhost:8080'
+    baseUrl: 'http://http://ec2-18-228-241-114.sa-east-1.compute.amazonaws.com:8080'
  };
+//Usar localhost para testes
